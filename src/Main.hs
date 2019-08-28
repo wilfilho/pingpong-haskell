7e647815b878d71ddbfd840f3ee283dbe0906911
Will Filho <will.flho@gmail.com>
2019-08-28 01:04:37 -0300

    Implement scoreboard

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,19 @@
 module Main where
 
+import System.Random
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 
 -- Define a estrutura fundamental dos dados no jogo.
 data PingPongGame = Game {
-    posicaoBola    :: (Float, Float),
-    velocidade     :: (Float, Float),
-    player1        :: Float,
-    player2        :: Float,
-    finalJogo      :: Float
+    posicaoBola      :: (Float, Float),
+    velocidade       :: (Float, Float),
+    player1          :: Float,
+    player2          :: Float,
+    pontuacaoPlayer1 :: Int,
+    pontuacaoPlayer2 :: Int,
+    finalJogo        :: Float
 } deriving Show
 
 -- Define a velocidade utilizada em todo o jogo.
@@ -27,14 +30,20 @@ background = black
 -- Define fps da animação
 fps = 30
 
+velocidades = [(196, 50), (-200, 246), (-100, 40)]
+
+velocidadeAleatoria = velocidades!!1
+
 -- Define desenho/estado inicial do jogo, ou seja, as posições iniciais dos
 -- dos jogadores/raquetes, bem como da bola.
 mundoInicial = Game {
     posicaoBola = (0, 0),
-    velocidade = velocidadePadrao,
+    velocidade = velocidadeAleatoria,
     player1 = 0,
     player2 = 0,
-    finalJogo = (-600)
+    finalJogo = (-600),
+    pontuacaoPlayer1 = 0,
+    pontuacaoPlayer2 = 0
 }
 
 corParedes = light (light (light black))
@@ -62,9 +71,9 @@ paredeRodape = desenhaParedeHorizontal 0 (-300)
 desenharRaquete x y cor = translate x y $ color cor $ rectangleSolid 10 70
 -- Desenha raquete do jogador1
 raqueteJogador1 mundo = desenharRaquete 290 novaPosicao green
- where novaPosicao = player1 mundo
+    where novaPosicao = player1 mundo
 raqueteJogador2 mundo = desenharRaquete (-290) novaPosicao blue
- where novaPosicao = player2 mundo
+    where novaPosicao = player2 mundo
 
 
 -- DESENHO DA BOLA --
@@ -72,7 +81,7 @@ posicaoHorizontal (x, y) = x
 posicaoVertical (x, y) = y
 
 desenharBola mundo = translate localizacaoHorizontalBola localizacaoVerticalBola $ color white $ circleSolid 10
- where
+    where
     localizacaoVerticalBola = posicaoVertical $ posicaoBola mundo
     localizacaoHorizontalBola = posicaoHorizontal $ posicaoBola mundo
 
@@ -88,15 +97,18 @@ renderizar mundo = pictures [
         raqueteJogador2 mundo,
         raqueteJogador1 mundo,
         desenharBola mundo,
-        translate (finalJogo mundo) 0 $ color green $ rectangleSolid 200 100
+        translate (finalJogo mundo) 0 $ color green $ rectangleSolid 200 100,
+        scale 0.3 0.3 (translate (700) (-800) $ color white $ text (show (pontuacaoPlayer2 mundo))),
+        scale 0.3 0.3 (translate (-800) (-800) $ color white $ text (show (pontuacaoPlayer1 mundo)))
     ]
 
 -- ANIMAÇÃO BOLA --
+
+golNaEsquerda (x, _) = x - padding <= -fromIntegral 600 / 2
+golNaDireita (x, _) = x + padding >=  fromIntegral 600 / 2
+
 colisaoLateral :: (Float, Float) -> Bool 
-colisaoLateral (x, _) = leftCollision || rightCollision
-    where
-        leftCollision    = x - padding <= -fromIntegral 600 / 2 
-        rightCollision   = x + padding >=  fromIntegral 600 / 2
+colisaoLateral (x, y) = golNaEsquerda (x, y) || golNaDireita(x, y)
 
 colisaoVertical :: (Float, Float) -> Bool 
 colisaoVertical (_, y) = topCollision || bottomCollision
@@ -123,6 +135,8 @@ analiseGol game = foiGol (posicaoBola game) && not (bateuRaqueteEsquerda game) &
 definirDirecao game = novoMundo
     where
         (atualX, atualY) = velocidade game
+        pontuacaoAtualPlayer1 = pontuacaoPlayer1 game
+        pontuacaoAtualPlayer2 = pontuacaoPlayer2 game
         novoY 
             | colisaoVertical (posicaoBola game) = -atualY
             | otherwise = atualY
@@ -130,11 +144,9 @@ definirDirecao game = novoMundo
             | colisaoLateral (posicaoBola game) = -atualX
             | otherwise = atualX
         novoMundo
-            | analiseGol game = game {
-                    velocidade = (0, 0),
-                    posicaoBola = (0, 0),
-                    finalJogo = 0
-                }
+            | analiseGol game && golNaEsquerda (posicaoBola game) = mundoInicial { pontuacaoPlayer2 = pontuacaoAtualPlayer2 + 1, pontuacaoPlayer1 = pontuacaoAtualPlayer1, velocidade = velocidadeAleatoria}
+            | analiseGol game && golNaDireita (posicaoBola game) = mundoInicial { pontuacaoPlayer1 = pontuacaoAtualPlayer1 + 1, pontuacaoPlayer2 = pontuacaoAtualPlayer2, velocidade = velocidadeAleatoria}
+        
             | otherwise = game { velocidade = (novoX, novoY) }
 
 
@@ -148,10 +160,10 @@ novaPosicaoBaixoRaquete posicaoAtual
     | otherwise = posicaoAtual
 
 eventosTeclado (EventKey (Char char) _ _ _) game
-    | char == 'w' = renderizarPlayer1 $ novaPosicaoCimaRaquete $ player1 game
-    | char == 's' = renderizarPlayer1 $ novaPosicaoBaixoRaquete $ player1 game
-    | char == 'o' = renderizarPlayer2 $ novaPosicaoCimaRaquete $ player2 game
-    | char == 'l' = renderizarPlayer2 $ novaPosicaoBaixoRaquete $ player2 game
+    | char == 'o' = renderizarPlayer1 $ novaPosicaoCimaRaquete $ player1 game
+    | char == 'l' = renderizarPlayer1 $ novaPosicaoBaixoRaquete $ player1 game
+    | char == 'w' = renderizarPlayer2 $ novaPosicaoCimaRaquete $ player2 game
+    | char == 's' = renderizarPlayer2 $ novaPosicaoBaixoRaquete $ player2 game
     where
         renderizarPlayer1 posicao = game { player1 = posicao}
         renderizarPlayer2 posicao = game { player2 = posicao}
